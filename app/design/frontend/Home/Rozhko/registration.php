7e54331517b8b65dc475a4404fb13dd15e550e8e
Stepan <stepan1q1@gmail.com>
2021-02-09 19:22:05 +0200

    magento + my theme (Rozhko)

---
+++ app/design/frontend/Home/Rozhko/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Home/rozhko', __DIR__);
